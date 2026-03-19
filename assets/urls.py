f1941e0b482d803e9fc815804c7e03f695aad0f0
str8zer0 <rpdimitrov@gmail.com>
2026-03-19 17:31:42 +0200

    feat: add base/partials templates and urls
    - create base.html layout
    - add navbar, footer, and messages partials
    - add basic urls structure

---
+++ assets/urls.py
@@ -0,0 +1,5 @@
+app_name = "assets"
+
+urlpatterns = [
+
+]
--- facility_manager/urls.py
+++ facility_manager/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("common.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("facilities/", include("facilities.urls")),
+    path("assets/", include("assets.urls")),
+    path("maintenance/", include("maintenance.urls")),
+    path("inventory/", include("inventory.urls")),
+    path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
 ]
