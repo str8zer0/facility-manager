d9a45db86387315bf9d4c52f28278c3c630c1282
str8zer0 <rpdimitrov@gmail.com>
2026-04-05 19:44:52 +0300

    Fix olacl dev settings

--- facility_manager/settings/dev.py
+++ facility_manager/settings/dev.py
@@ -3,6 +3,20 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = []
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
+
 # Development email backend with MailHog
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "localhost"
